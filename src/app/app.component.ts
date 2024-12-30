bab3ac6583c4d876fc7418d82a437e882f974fcc
Alexandru Dragomir <a-dragomir@outlook.com>
2024-12-29 20:18:27 -0500

    updated generic animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
 import { FooterComponent } from './components/common/footer/footer.component';
-import {CustomLottieComponent} from './components/common/custom-lottie/custom-lottie.component';
 import {AnimationType} from './enums/animation-type-enum';
 import {AnimationDirective} from './directives/animation.directive';
+import {CustomLottieComponent} from './components/common/custom-lottie/custom-lottie.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, CustomLottieComponent, AnimationDirective],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, AnimationDirective, CustomLottieComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
