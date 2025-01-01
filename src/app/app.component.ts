7ffddcb879dbccd357266076b503b544142cc566
Alexandru Dragomir <a-dragomir@outlook.com>
2024-12-31 21:24:21 -0500

    cleaned app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
 import { FooterComponent } from './components/common/footer/footer.component';
-import {AnimationType} from './enums/animation-type-enum';
 import {CustomGifDocComponent} from './components/custom-gif-doc/custom-gif-doc.component';
 import {AnimationDocComponent} from './components/animation-doc/animation-doc.component';
 import {CustomVideoDocComponent} from './components/custom-video-doc/custom-video-doc.component';
@@ -18,15 +17,4 @@ import {CustomLottieDocComponent} from './components/custom-lottie-doc/custom-lo
 })
 export class AppComponent  {
   title = 'alexdr-ai';
-
-
-  lottieRocket = {
-    path: 'assets/lotties/Animation - fix-rocket.json',
-  };
-
-  lottieOptionsCropped = {
-    path: 'assets/lotties/CroppedAIBlueish.json',
-  };
-
-  protected readonly AnimationType = AnimationType;
 }
