df6c2852507c8fdda0a19027c1d70a491b33bf35
Alexandru Dragomir <a-dragomir@outlook.com>
2025-01-01 15:58:50 -0500

    updated footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,16 @@ import {CustomGifDocComponent} from './components/custom-gif-doc/custom-gif-doc.
 import {CustomVideoDocComponent} from './components/custom-video-doc/custom-video-doc.component';
 import {CustomLottieDocComponent} from './components/custom-lottie-doc/custom-lottie-doc.component';
 import {HomeComponent} from './components/home/home.component';
+import {AboutComponent} from './components/about/about.component';
+import {ServicesComponent} from './components/services/services.component';
+import {ContactComponent} from './components/contact/contact.component';
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Home page route
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'services', component: ServicesComponent },
   { path: 'animation-doc', component: AnimationDocComponent },
   { path: 'custom-gif-doc', component: CustomGifDocComponent },
   { path: 'custom-video-doc', component: CustomVideoDocComponent },
